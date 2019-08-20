f638da0e0ef0a66243d9568cc4a2b380945b6e80
Luis Carlin <luiscarlin@gmail.com>
2019-08-20 12:51:46 -0400

    get books based on title

--- scraper.js
+++ scraper.js
@@ -1,17 +1,26 @@
-const request = require('request');
+//request('https://cml.bibliocommons.com/v2/search?query=neal+stephenson&searchType=author', function(err, res, body) {  
+//request('https://cml.bibliocommons.com/v2/search?query=brandon+sanderson&searchType=author', function(err, res, body) {  
+// https://cml.bibliocommons.com/v2/search?query=cities+of+the+red+night&searchType=title
+// request(`https://cml.bibliocommons.com/v2/search?query=${firstName}+${lastName}&searchType=author`, function(err, res, body) {
+
+const request = require('request')
 const cheerio = require('cheerio')
 
-//request('https://cml.bibliocommons.com/v2/search?query=neal+stephenson&searchType=author', function(err, res, body) {  
+const title = "the hobbit"
+const authorLastName = "tolkien"
+// const authorFirstName = "Neil"
 
-//request('https://cml.bibliocommons.com/v2/search?query=brandon+sanderson&searchType=author', function(err, res, body) {  
-
-const firstName = "stephen"
-const lastName = "king"
-
-request(`https://cml.bibliocommons.com/v2/search?query=${firstName}+${lastName}&searchType=author`, function(err, res, body) {  
+request(`https://cml.bibliocommons.com/v2/search?query=${encodeURI(title)}&searchType=title`, function(err, res, body) { 
     const $ = cheerio.load(body)
     const root = JSON.parse($('script[type="application/json"]')[0].children[0].data)
-    const bibs = Object.values(root.entities.bibs)
 
-    console.log(bibs.map(item => item.briefInfo.title))
+    const bibs = Object.values(root.entities.bibs)
+    const listBooks = [... new Set(bibs.map(item => ({authors: item.briefInfo.authors || [], title: item.briefInfo.title, format: item.briefInfo.format})))]
+
+    const authorFilteredBooks = listBooks.filter(book => book.authors.length > 0 && book.authors[0].toUpperCase().includes(authorLastName.toUpperCase()))
+    const formatFilteredBooks = authorFilteredBooks.filter(book => book.format == "BK" || book.format == "AB")
+
+    console.log(formatFilteredBooks)
+
+    // https://cml.bibliocommons.com/v2/search/availability/S105C2258827?query=norse+mythology&searchType=title&sort=author
 });
